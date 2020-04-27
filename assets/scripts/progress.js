d06b75f53970d2faebc0323036b17c79a5f76503
Diana García <diana.eli.gg@gmail.com>
2020-04-26 23:40:55 -0500

    song

--- assets/scripts/progress.js
+++ assets/scripts/progress.js
@@ -2,25 +2,26 @@ var Progress = {
     bar : null,
     index : 0,
     imgList : [],
+    assetsLength: 0,
+    song : "",
     init: function(){
-        var box = document.getElementById("progressBox");
-        var prog = document.createElement("progress");
-        box.appendChild(prog);
-        console.log(prog)
-        this.bar = prog;
+        // var box = document.getElementById("progressBox");
+        // var prog = document.createElement("progress");
+        // box.appendChild(prog);
+        // console.log(prog)
+        // this.bar = prog;
     },
 
     loadImages: function(paths) {
         var self = this,
             i, length = paths.length;
-        this.bar.max = length;
+        // this.bar.max = length;
+        this.assetsLength = paths.length+1;
         for (i=0;i<length;i++){
             var img = new Image();
             img.onload = function() {
-                self.increase();
-                if (self.index >= length){
-                    self.done();
-                }
+                self.increase('gif');
+                
             };
             img.src = paths[i];
             this.imgList.push(img);
@@ -28,15 +29,24 @@ var Progress = {
         }
     },
 
-    increase: function(){
-        this.index++;
-        this.bar.value = this.index;
-
+    loadAudio: function(path) {
+        this.song = new Audio(path);
+        this.song.addEventListener('canplaythrough', () => this.increase('audio'));
     },
-
+    
+    increase: function(source){
+        this.index++;
+        console.log(source, this.index)
+        if (this.index >= this.assetsLength){
+            this.done();
+        }
+        // this.bar.value = this.index;
+    },
+    
     done : function() {
         console.log("I am done loading");
-        this.bar.style.display = "none";
+        this.song.play();
+        // this.bar.style.display = "none";
         var showBox = document.getElementById("show");
         for (var i =0, l = this.index;i<l;i++) {
             showBox.appendChild(this.imgList[i]);
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,10 +11,11 @@ var imagePaths = [
     "./assets/images/10.gif",
     "./assets/images/11.gif",
     "./assets/images/12.gif",
-    "./assets/images/13.gif"
+    "./assets/images/13.gif",
     // "./assets/music/pepepepepepe.mp3"
 ];
 
 Progress.init();
 Progress.loadImages(imagePaths);
+Progress.loadAudio("./assets/music/pepepepepepe.mp3");
 
