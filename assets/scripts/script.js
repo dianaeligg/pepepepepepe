b3febbe3298ef3e2bdad536b405f8c6403479b61
Diana García <diana.eli.gg@gmail.com>
2020-05-24 00:06:52 -0500

    Added protection for Safari

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,30 @@
 let imagePaths = [];
 let howMany = 0;
-if (navigator.connection.type === "wifi" || 
-    navigator.connection.type === "ethernet" ){
+if (navigator.connection) {
+  if (
+    navigator.connection.type === "wifi" ||
+    navigator.connection.type === "ethernet"
+  ) {
     howMany = 19;
-}
-else{
+  } else {
     howMany = 7;
+  }
+} else {
+  howMany = 19;
 }
 
-for (i = 1; i < howMany; i++){
-    imagePaths.push(`./assets/images/${i}.gif`);
+for (i = 1; i < howMany; i++) {
+  imagePaths.push(`./assets/images/${i}.gif`);
 }
 
 let backgrounds = [
-    'rgb(223, 156, 141)',
-    'rgb(241, 231, 210)',
-    'rgb(0, 0, 0)',
-    'rgb(191, 174, 134)',
-    'rgb(62,80,94)',
-    'rgb(255,255,255)'
-]
+  "rgb(223, 156, 141)",
+  "rgb(241, 231, 210)",
+  "rgb(0, 0, 0)",
+  "rgb(191, 174, 134)",
+  "rgb(62,80,94)",
+  "rgb(255,255,255)",
+];
 
 Progress.init();
 Progress.loadAudio("./assets/music/pepepepepepe.mp3");
@@ -32,42 +37,40 @@ let dance = document.querySelector(".dance");
 let bigButton = document.querySelector(".bigButton");
 
 bigButton.addEventListener("click", () => {
-    click();
+  click();
 });
 
 dance.addEventListener("click", () => {
-    click();
+  click();
 });
 
-function toggleMusic () {
-    if (playing) Progress.song.pause();
-    else Progress.song.play();
+function toggleMusic() {
+  if (playing) Progress.song.pause();
+  else Progress.song.play();
 }
 
-function click (){
-    show.style["align-items"] = "center";
-    if (playing){
-        dance.style.display = "none";
-        bigButton.style.display = "flex";
-    }else{
-        let ran = Math.floor(Math.random() * imagePaths.length);
-        if (ran < 5){
-            dance.style.width = "auto";
-            dance.style.height = "auto";
-            body.style.backgroundColor = backgrounds[ran];
-            if (ran === 2){
-                show.style["align-items"] = "flex-end";
-            }
-        }
-        else{
-            dance.style.width = "100%";
-            dance.style.height = "100%";
-        }
-        dance.setAttribute("src", imagePaths[ran]);
-        dance.style.display = "flex";
-        bigButton.style.display = "none";
+function click() {
+  show.style["align-items"] = "center";
+  if (playing) {
+    dance.style.display = "none";
+    bigButton.style.display = "flex";
+  } else {
+    let ran = Math.floor(Math.random() * imagePaths.length);
+    if (ran < 5) {
+      dance.style.width = "auto";
+      dance.style.height = "auto";
+      body.style.backgroundColor = backgrounds[ran];
+      if (ran === 2) {
+        show.style["align-items"] = "flex-end";
+      }
+    } else {
+      dance.style.width = "100%";
+      dance.style.height = "100%";
     }
-    toggleMusic();
-    playing = !playing;
+    dance.setAttribute("src", imagePaths[ran]);
+    dance.style.display = "flex";
+    bigButton.style.display = "none";
+  }
+  toggleMusic();
+  playing = !playing;
 }
-
